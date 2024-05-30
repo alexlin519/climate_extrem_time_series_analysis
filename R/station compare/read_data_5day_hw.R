9edf23c857855c4bb991db3565ac29d04347dafd
alexlin519 <alex273988@gmail.com>
2024-05-30 12:02:21 -0700

    get 5 days for line segment as well

--- R/station compare/read_data_5day_hw.R
+++ R/station compare/read_data_5day_hw.R
@@ -5,13 +5,13 @@ library(purrr)
 library(tidyr)
 
 # Define file paths
-file_paths <- c("../output/YVR_heatmap_3_dayHW.csv",
-                "../output/Kamloops_heatmap_3_dayHW.csv",
-                "../output/Prince_George_heatmap_3_dayHW.csv")
+file_paths <- c("../output/YVR_heatmap_5_dayHW.csv",
+                "../output/Kamloops_heatmap_5_dayHW.csv",
+                "../output/Prince_George_heatmap_5_dayHW.csv")
 # Function to read and select necessary columns
 read_and_select <- function(file_path) {
   read.csv(file_path) 
 }
 
 # Read and combine all datasets
-df_line_seg_3day <- map_dfr(file_paths, read_and_select)
+df_line_seg_5day <- map_dfr(file_paths, read_and_select)
