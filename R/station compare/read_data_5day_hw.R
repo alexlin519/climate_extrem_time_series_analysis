30ba7d86ac2affad023bb3a439b36287c2b516eb
alexlin519 <alex273988@gmail.com>
2024-06-17 16:57:44 -0700

    finish the line segment for fn

--- R/station compare/read_data_5day_hw.R
+++ R/station compare/read_data_5day_hw.R
@@ -11,7 +11,8 @@ file_paths <- c("../output/YVR_heatmap_5_dayHW.csv",
                 "../output/era5_YVR_heatmap_5_dayHW.csv",
                 "../output/Kelowna_heatmap_5_dayHW.csv",
                 "../output/Abbotsford_heatmap_5_dayHW.csv",
-                "../output/Penticton_heatmap_5_dayHW.csv")
+                "../output/Penticton_heatmap_5_dayHW.csv",
+                "../output/FortNelson_heatmap_5_dayHW.csv")
 # Function to read and select necessary columns
 read_and_select <- function(file_path) {
   read.csv(file_path) 
